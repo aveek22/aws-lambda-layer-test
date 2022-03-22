19cfbea047dcff545b1e9b8a78e7194c58a6213f
Aveek <aveek.d22@gmail.com>
2022-03-22 10:18:11 +0100

    Added lambda function code.

---
+++ lambda-function-2/app.py
@@ -0,0 +1,16 @@
+""" This is an AWS Lambda function to demonstrate the import of Lambda Layers
+"""
+
+# from lambdalayer import lambdalayer
+
+
+def main(event, context):
+    """Handler method for the AWS Lambda Function."""
+
+    print(f"*** Lambda 1: Execution of AWS Lambda function starts. ***")
+    print(f"*** Lambda 1: Trying to invoke Lambda Layer. ***")
+
+    # lambdalayer.import_layer()
+
+    print(f"*** Lambda 1: Invoked method from Lambda Layer. ***")
+    print(f"*** Lambda 1: Execution of AWS Lambda function ends. ***")
