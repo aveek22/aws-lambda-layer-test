34d1d929e5f9de92387b1038ead9b79016124aed
Aveek <aveek.d22@gmail.com>
2022-03-22 10:42:31 +0100

    Added support for Lambda Layer

---
+++ terraform/lambda_layer/lambda_layer.tf
@@ -0,0 +1,15 @@
+locals {
+  lambda-layer-zip-location = "../app/lambda-layer/python.zip"
+}
+
+resource "aws_lambda_layer_version" "test_lambda_layer" {
+  filename   = local.lambda-layer-zip-location
+  layer_name = "test_lambda_layer"
+  source_code_hash    = "${filebase64sha256(local.lambda-layer-zip-location)}"
+  compatible_runtimes = ["python3.6","python3.7","python3.8","python3.9"]
+}
+
+output "test_lambda_layer_arn" {
+  description = "The ARN of the Lambda Layer"
+  value       = aws_lambda_layer_version.test_lambda_layer.arn
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
 module "lambda_1" {
     source  = "../terraform/lambda_1"
 }
+
+module "lambda_layer" {
+    source  = "../terraform/lambda_layer"
+}
