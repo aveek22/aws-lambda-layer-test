86358af8263f79853be5bf2b841145d35da80384
Aveek <aveek.d22@gmail.com>
2022-03-23 16:25:33 +0100

    Added Lambda Layer to main module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,13 @@
 module "lambda_1" {
     source  = "../terraform/lambda_1"
+    lambda_layer_arn = "${module.lambda_layer.test_lambda_layer_arn}"
 }
 
 module "lambda_2" {
     source  = "../terraform/lambda_2"
+    lambda_layer_arn = "${module.lambda_layer.test_lambda_layer_arn}"
 }
 
-# module "lambda_layer" {
-#     source  = "../terraform/lambda_layer"
-# }
+module "lambda_layer" {
+    source  = "../terraform/lambda_layer"
+}
